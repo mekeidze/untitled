00229299fdfde2330d78f4f8d0d898a91493581f
аслан мекеидзе <mekeidzeaslan9@gmail.com>
2022-02-14 20:20:29 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     /*         for (int i = 0; i < 10; i++){
+                 System.out.println("i");
+        for (int i = 0; i < 10;i++){
+                if (i == 5) {
+                    continue;
+           }
+        //              System.out.println(i);
+        //        for (int i = 0; i < 10;i++){
+        //          if (i == 5) {
+        //            break;
+        //}
+        ///      System.out.println(i);
+   // }
+      //  wile (i<10) {
+        //    if (i == 5) {
+ /*   int i=0;
+      do{
+          System.out.println(i);
+          i++;
+
+      }while(i <10);
+
+  */
+        for (int i = 0; i < 17; i++) {
+            if (i == 17) {
+
+                {
+            System.out.println(i);
+        }
+    }
